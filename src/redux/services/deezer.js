449c7334ad10cde978ff4cd334ab5d21ee0fcc69
Andrei <dandrei.web@gmail.com>
2023-05-16 21:03:14 +0300

    update

--- src/redux/services/deezer.js
+++ src/redux/services/deezer.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const deezerApi = createApi({
   reducerPath: "deezerApi", // name of the api
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.deezer.com",
-    // baseUrl: "https://cors-anywhere.herokuapp.com/https://api.deezer.com", // enable to work deezerapi on local
+    // baseUrl: "https://api.deezer.com",
+    baseUrl: "https://cors-anywhere.herokuapp.com/https://api.deezer.com",
   }),
   prepareHeaders: (headers) => {
     headers.set("method", "GET");
